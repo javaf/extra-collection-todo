03efcac0500d7b0fa6e13ed26d122ff9ceac2d3f
Subhajit Sahu <wolfram77@gmail.com>
2015-02-25 11:44:49 +0530

    Coll class ready

--- src/org/data/Coll.java
+++ src/org/data/Coll.java
@@ -1,14 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// @wolfram77
 package org.data;
 
-/**
- *
- * @author verizon
- */
+// required modules
+import java.util.*;
+
+
+
 public class Coll {
     
+    // Map (arr)
+    // - converts an array of key, value pairs to map
+    public static Map map(Object[] arr) {
+        Map map = new HashMap();
+        for(int i=0; i<arr.length; i+=2)
+            map.put(arr[i], arr[i+1]);
+        return map;
+    }
+    
+    
+    // add some more basic functionality here
 }
