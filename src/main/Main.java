f38125f8d9e99e1f3f029f8adf9a07a1c0613e4d
Subhajit Sahu <wolfram77@gmail.com>
2015-02-25 11:48:26 +0530

    Coll test complete

---
+++ src/main/Main.java
@@ -0,0 +1,17 @@
+// @wolfram77
+package main;
+
+// required modules
+import java.util.*;
+import org.data.*;
+
+
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Object[] arr = {"topic", "area51", "aliens", 0};
+        Map map = Coll.map(arr);
+        System.out.println(map);
+    }
+}
